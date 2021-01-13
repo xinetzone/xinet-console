ac35689247aaeeffe8df6f77ed61627ec83ae82f
xinetzone <xinzone@outlook.com>
2021-01-13 17:12:59 +0800

    初创

---
+++ script/console.js
@@ -0,0 +1,39 @@
+var geval = eval
+
+function createInput() {
+    var inputDiv = document.createElement('div')
+    var inputPara = document.createElement('p')
+    var inputForm = document.createElement('input')
+
+    inputDiv.setAttribute('class', 'input')
+    inputPara.textContent = '>'
+    inputDiv.appendChild(inputPara)
+    inputDiv.appendChild(inputForm)
+    document.body.appendChild(inputDiv)
+    inputDiv.focus()
+
+    inputForm.addEventListener('change', executeCode)
+}
+
+function executeCode(e) {
+    try {
+        var result = geval(e.target.value)
+    } catch (e) {
+        var result = 'error — ' + e.message
+    }
+
+    var outputDiv = document.createElement('div')
+    var outputPara = document.createElement('p')
+
+    outputDiv.setAttribute('class', 'output')
+    outputPara.textContent = 'Result: ' + result
+    outputDiv.appendChild(outputPara)
+    document.body.appendChild(outputDiv)
+
+    e.target.disabled = true
+    e.target.parentNode.style.opacity = '0.5'
+
+    createInput()
+}
+
+createInput();
